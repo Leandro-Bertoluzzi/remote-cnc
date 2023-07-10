d9dbf813e9c1da1bc9a4895bf189aa053fb22ed9
Leandro Bertoluzzi <leajb10@gmail.com>
2023-07-10 19:37:08 -0300

    feat: Added DB migrations

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from PyQt5.QtWidgets import QApplication
+from MainWindow import MainWindow
 from config import suppressQtWarnings
 import sys
 
 if __name__ == '__main__':
     suppressQtWarnings()
     app = QApplication(sys.argv)
-
-    from MainWindow import MainWindow
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec())
