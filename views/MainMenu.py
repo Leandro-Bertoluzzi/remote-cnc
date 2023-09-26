32b822dadc6ca214537af99a1d07b3cc2a49c21b
Leandro Bertoluzzi <leajb10@gmail.com>
2023-09-26 17:57:37 -0300

    style: Arranged main menu buttons in a grid layout

--- views/MainMenu.py
+++ views/MainMenu.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QGridLayout
 from PyQt5.QtCore import Qt
 from components.MenuButton import MenuButton
 from views.FilesView import FilesView
@@ -11,14 +11,14 @@ class MainMenu(QWidget):
     def __init__(self, parent=None):
         super(MainMenu, self).__init__(parent)
 
-        layout = QVBoxLayout()
-        layout.addWidget(MenuButton('Ver estado de tareas', goToView=TasksView, parent=self))
-        layout.addWidget(MenuButton('Monitorizar equipo'))
-        layout.addWidget(MenuButton('Administrar archivos', goToView=FilesView, parent=self))
-        layout.addWidget(MenuButton('Control manual y calibración'))
-        layout.addWidget(MenuButton('Administrar solicitudes', goToView=RequestsView, parent=self))
-        layout.addWidget(MenuButton('Administrar usuarios', goToView=UsersView, parent=self))
-        layout.addWidget(MenuButton('Administrar inventario', goToView=InventoryView, parent=self))
+        layout = QGridLayout()
+        layout.addWidget(MenuButton('Ver estado de tareas', goToView=TasksView, parent=self), 1, 1)
+        layout.addWidget(MenuButton('Monitorizar equipo'), 1, 2)
+        layout.addWidget(MenuButton('Administrar archivos', goToView=FilesView, parent=self), 1, 3)
+        layout.addWidget(MenuButton('Calibración'), 1, 4)
+        layout.addWidget(MenuButton('Administrar solicitudes', goToView=RequestsView, parent=self), 2, 1)
+        layout.addWidget(MenuButton('Administrar usuarios', goToView=UsersView, parent=self), 2, 2)
+        layout.addWidget(MenuButton('Administrar inventario', goToView=InventoryView, parent=self), 2, 3)
         layout.setAlignment(Qt.AlignCenter)
         self.setLayout(layout)
 
