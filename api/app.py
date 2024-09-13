425268a4e959bc621e621b67b4b0549a0cd0d73c
Leandro Bertoluzzi <leajb10@gmail.com>
2024-09-12 22:07:41 -0300

    feat(api): Added healthcheck endpoint

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from routes.cncRoutes import cncRoutes
 from routes.fileRoutes import fileRoutes
 from routes.materialRoutes import materialRoutes
 from routes.monitorRoutes import monitorRoutes
+from routes.rootRoutes import rootRoutes
 from routes.toolRoutes import toolRoutes
 from routes.taskRoutes import taskRoutes
 from routes.userRoutes import userRoutes
@@ -19,17 +20,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/")
-def read_main():
-    return {"message": "Hello World from FastAPI"}
-
-
 # Routes
 app.include_router(cncRoutes)
 app.include_router(fileRoutes)
 app.include_router(materialRoutes)
 app.include_router(monitorRoutes)
+app.include_router(rootRoutes)
 app.include_router(toolRoutes)
 app.include_router(taskRoutes)
 app.include_router(userRoutes)
